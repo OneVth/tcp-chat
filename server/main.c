dc311e06777fbbffb95ec503f07f0138f8c6e505
OneVth <onebrotravel@gmail.com>
2025-06-30 19:37:27 +0900

    refactor(server): replace hardcoded values with PORT and BUFFER_SIZE macros

--- server/main.c
+++ server/main.c
@@ -4,12 +4,15 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+#define BUFFER_SIZE 1024
+#define PORT 25000
+
 int main(void)
 {
     int server_fd, client_fd;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
-    char buffer[1024] = {0};
+    char buffer[BUFFER_SIZE] = {0};
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1)
@@ -20,7 +23,7 @@ int main(void)
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(25000);
+    server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
@@ -37,7 +40,7 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-    printf("Echo server is running on port %d...\n", 25000);
+    printf("Echo server is running on port %d...\n", PORT);
 
     client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
     if (client_fd == -1)
@@ -51,7 +54,7 @@ int main(void)
 
     while (1)
     {
-        ssize_t n = recv(client_fd, buffer, 1024 - 1, 0);
+        ssize_t n = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
         if (n <= 0)
         {
             printf("Client disconnected.\n");
